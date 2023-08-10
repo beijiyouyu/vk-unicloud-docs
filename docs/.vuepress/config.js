1f859c877e273f51bf649e476cb58055554e19c2
VK <370725567@qq.com>
2023-08-10 20:34:23 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -69,7 +69,7 @@ module.exports = {
   plugins: [
     '@vuepress/back-to-top',
     '@vuepress/active-header-links',
-    'vuepress-plugin-smooth-scroll',
+    //'vuepress-plugin-smooth-scroll',
     [
       '@vuepress/medium-zoom',
       {
